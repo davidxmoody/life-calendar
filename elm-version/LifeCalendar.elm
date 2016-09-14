81b50509e9f04e9dc5836c573f14f7014b6a4609
David Moody <david@davidxmoody.com>
2016-09-14 13:52:55 +0100

    Add some rough Elm date manipulation code

--- elm-version/LifeCalendar.elm
+++ elm-version/LifeCalendar.elm
@@ -1,12 +1,22 @@
 import Html exposing (..)
 import Html.App as App
+import Date exposing (Date)
+import Date.Extra.Utils exposing (dayList, unsafeFromString)
 
 main = App.beginnerProgram { model = model, view = view, update = update }
 
+-- Data
+
+-- TODO this is what I think I will need...
+
+type alias Week =
+  { startDate : Date
+  , endDate : Date
+  , era : String
+  }
+
 
 -- Model
-
-type alias Date = String
 
 type alias Model = 
   { birthDate: Date
@@ -15,7 +25,7 @@ type alias Model =
   }
 
 model : Model
-model = Model "1990-07-04" "2016-09-13" "2016-09-13"
+model = Model (unsafeFromString "1990-07-04") (unsafeFromString "2016-09-13") (unsafeFromString "2016-09-13")
 
 
 -- Update
@@ -30,4 +40,4 @@ update msg model = case msg of
 -- View
 
 view : Model -> Html Msg
-view model = div [] [text ("Selected date: " ++ model.selectedDate)]
+view model = div [] [text ("Selected date: " ++ toString (dayList 5 model.selectedDate))]
