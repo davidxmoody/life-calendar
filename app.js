ae523ba9f611e31ea2a30af72c0bc0e0b6a9d369
David Moody <david@davidxmoody.com>
2016-10-08 17:16:15 +0100

    Use new week-based data format

--- app.js
+++ app.js
@@ -3,30 +3,34 @@ const express = require("express")
 const nunjucks = require("nunjucks")
 const moment = require("moment")
 
-const generateWeeks = require('./src/generateWeeks')
-const getDataForWeek = require('./getDataForWeek')
-const birthDate = require('./data/birthDate')
-const eras = require('./data/eras')
+const generateWeeks = require("./src/generateWeeks")
+const unsafeGetDataForWeek = require("./unsafeGetDataForWeek")
+const birthDate = require("./data/birthDate")
+const eras = require("./data/eras")
 
-const currentDate = moment().format('YYYY-MM-DD')
+const currentDate = moment().format("YYYY-MM-DD")
 const weeks = generateWeeks({currentDate, birthDate, eras})
 
 const app = express()
 
 app.set("views", path.join(__dirname, "./views"))
 
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   autoescape: true,
   express: app,
-});
+})
 
 app.get("/", (req, res) => {
   res.render("index.njk", {weeks})
 })
 
 app.get("/weeks/:week", (req, res) => {
-  const weekData = getDataForWeek(req.params.week)
-  res.render("week.njk", weekData)
+  try {
+    const weekData = unsafeGetDataForWeek(req.params.week)
+    res.render("week.njk", weekData)
+  } catch (e) {
+    res.status(400).render("error.njk", {error: e.toString()})
+  }
 })
 
 app.listen("5000", () => {
