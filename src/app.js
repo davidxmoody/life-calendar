6d55410b83bfb8df8390d1f9699cd8b14db049a7
David Moody <david@davidxmoody.com>
2017-01-19 20:55:27 +0000

    Try out new era groups organisation

--- src/app.js
+++ src/app.js
@@ -4,24 +4,26 @@ const nunjucks = require("nunjucks")
 const moment = require("moment")
 
 const generateWeeks = require("./generate-weeks")
-const {birthDate, deathDate, eras} = require("../life-data.json")
+const {birthDate, deathDate, eraGroups} = require("../life-data.json")
 
 const currentDate = moment().format("YYYY-MM-DD")
 
 // Hack
-for (const era of eras) {
-  if (era.startDate === "today") era.startDate = currentDate
-  if (era.endDate === "today") era.endDate = currentDate
+for (const eraGroup of eraGroups) {
+  for (const era of eraGroup.eras) {
+    if (era.startDate === "today") era.startDate = currentDate
+    if (era.endDate === "today") era.endDate = currentDate
+  }
 }
 
-const weeks = generateWeeks({currentDate, birthDate, deathDate, eras})
+const weeks = generateWeeks({birthDate, deathDate})
 
 const app = express()
 
 nunjucks.configure(path.join(__dirname, "./views"), {autoescape: true, express: app})
 
 app.get("/", (req, res) => {
-  res.render("index.njk", {currentDate, weeks, eras})
+  res.render("index.njk", {currentDate, weeks, eraGroups})
 })
 
 app.listen("5000", () => {
--- src/generate-weeks.js
+++ src/generate-weeks.js
@@ -12,31 +12,15 @@ function getYearNum(firstWeekStart, weekStart) {
   return Math.abs(moment(weekStart).diff(firstWeekStart, "years"))
 }
 
-function getEra(eras, weekStart) {
-  let selectedEra = eras[0]
+module.exports = ({birthDate, deathDate}) => {
+  const weeks = [getWeekStart(birthDate)]
 
-  for (const era of eras) {
-    if (era.startDate <= weekStart) {
-      selectedEra = era
-    }
-  }
-
-  return selectedEra
-}
-
-module.exports = ({birthDate, deathDate, eras}) => {
-  const firstWeekStart = getWeekStart(birthDate)
-  let weeks = [firstWeekStart]
   while (weeks[weeks.length - 1] <= deathDate) {
     weeks.push(getNextWeekStart(weeks[weeks.length - 1]))
   }
 
-  weeks = weeks.map(startDate => ({startDate}))
-
-  for (const week of weeks) {
-    week.yearNum = getYearNum(birthDate, week.startDate)
-    week.color = getEra(eras, week.startDate).color
-  }
-
-  return weeks
+  return weeks.map((startDate) => ({
+    startDate,
+    yearNum: getYearNum(birthDate, startDate),
+  }))
 }
