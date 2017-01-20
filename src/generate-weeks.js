80436fad327bc7a8f538788e8bcfc80bce51cb9e
David Moody <david@davidxmoody.com>
2017-01-20 20:51:24 +0000

    Playing around with colors

--- src/generate-weeks.js
+++ src/generate-weeks.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-constant-condition */
 
 const moment = require("moment")
+const col = require("tinycolor2")
 
 function getWeekStart(date) {
   return moment(date).isoWeekday(1).format("YYYY-MM-DD")
@@ -14,16 +15,24 @@ function getYearNum(firstWeekStart, weekStart) {
   return Math.abs(moment(weekStart).diff(firstWeekStart, "years"))
 }
 
-function getColor(eras, weekStart) {
+function getEra(eras, weekStart) {
   let selectedEra = eras[0]
+  let subEraIndex = 0
 
   for (const era of eras) {
     if (era.startDate <= weekStart) {
       selectedEra = era
+      if (era.subEras) {
+        era.subEras.forEach((subEra, index) => {
+          if (subEra.startDate <= weekStart) {
+            subEraIndex = index
+          }
+        })
+      }
     }
   }
 
-  return selectedEra.color
+  return [selectedEra, subEraIndex]
 }
 
 module.exports = ({birthDate, deathDate, eras}) => {
@@ -36,10 +45,27 @@ module.exports = ({birthDate, deathDate, eras}) => {
     weeks.push(nextWeek)
   }
 
-  weeks = weeks.map((startDate) => ({
-    startDate,
-    color: getColor(eras, startDate),
-  }))
+  let lastEra = eras[0]
+  let lastSubEraIndex = 0
+  let colorModifier = 1
+  weeks = weeks.map((startDate, index) => {
+    const [era, subEraIndex] = getEra(eras, startDate, index, weeks.length)
+    if (era !== lastEra || subEraIndex !== lastSubEraIndex) {
+      console.log("reset era", lastEra.name, era.name, lastSubEraIndex, subEraIndex)
+      lastEra = era
+      lastSubEraIndex = subEraIndex
+      colorModifier = 1
+    } else {
+      colorModifier = Math.max(0, colorModifier - (1 / 20))
+    }
+
+    const color = col(era.color).brighten(15 * (1 - colorModifier)).toRgbString()
+
+    return {
+      startDate,
+      color,
+    }
+  })
 
   const calendar = {
     birthDate,
