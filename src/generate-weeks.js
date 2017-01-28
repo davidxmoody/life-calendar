a69c5dc9e5ca87e3a7fad20e52e9302acb805d25
David Moody <david@davidxmoody.com>
2017-01-28 14:41:50 +0000

    Try out new running data visualisation

--- src/generate-weeks.js
+++ src/generate-weeks.js
@@ -4,6 +4,53 @@ const moment = require("moment")
 const col = require("tinycolor2")
 const probabilityOfSurvival = require("./probability-of-survival")
 
+const runningData = {
+  "2016-03-21": 4,
+  "2016-03-28": 4,
+  "2016-04-04": 5,
+  "2016-04-11": 5,
+  "2016-04-18": 4,
+  "2016-04-25": 7,
+  "2016-05-02": 6,
+  "2016-05-09": 4,
+  "2016-05-16": 4,
+  "2016-05-23": 4,
+  "2016-05-30": 7,
+  "2016-06-06": 4,
+  "2016-06-13": 4,
+  "2016-06-20": 5,
+  "2016-06-27": 4,
+  "2016-07-04": 1,
+  "2016-07-11": 4,
+  "2016-07-18": 3,
+  "2016-07-25": 2,
+  "2016-08-01": 5,
+  "2016-08-08": 3,
+  "2016-08-15": 7,
+  "2016-08-22": 11,
+  "2016-08-29": 2.5,
+  "2016-09-12": 10.5,
+  "2016-09-19": 9,
+  "2016-09-26": 2.5,
+  "2016-10-03": 8.5,
+  "2016-10-10": 5,
+  "2016-10-17": 3.5,
+  "2016-10-24": 2.5,
+  "2016-10-31": 1,
+  "2016-11-07": 5.5,
+  "2016-11-14": 7,
+  "2016-11-21": 8,
+  "2016-11-28": 13,
+  "2016-12-05": 5,
+  "2016-12-12": 10,
+  "2016-12-19": 7.5,
+  "2016-12-26": 14,
+  "2017-01-02": 2.5,
+  "2017-01-09": 10,
+  "2017-01-16": 7.5,
+  "2017-01-23": 5,
+}
+
 function getWeekStart(date) {
   return moment(date).isoWeekday(1).format("YYYY-MM-DD")
 }
@@ -56,7 +103,8 @@ module.exports = ({currentDate, birthDate, deathDate, eras}) => {
     const [era, subEraIndex] = getEra(eras, startDate, index, weeks.length)
     const start = era.subEras ? era.subEras[subEraIndex].startDate || era.startDate : era.startDate
     const end = era.subEras ? (era.subEras[subEraIndex + 1] || {}).startDate || era.endDate || moment().format("YYYY-MM-DD") : era.endDate || moment().format("YYYY-MM-DD")
-    const intensity = 1 - ratioBetweenDates(start, end, startDate)
+    let intensity = 1 - ratioBetweenDates(start, end, startDate)
+    intensity = 1 - ((Math.min(1, runningData[startDate] / 12)) || 0)
 
     let color = col.mix(era.startColor, era.endColor, 100 * (intensity)).toRgbString()
 
