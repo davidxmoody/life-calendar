72f0e3565fc89ab4d79e7621b27400f658bdcbda
David Moody <david@davidxmoody.com>
2019-08-24 11:49:17 +0100

    Remove old-client
    Everything I wanted to keep has now been copied across.

--- old-client/src/fetchEntriesForWeek.ts
+++
@@ -1,7 +0,0 @@
-import {Entry} from "./types"
-
-const REMOTE_URL = "http://localhost:3001"
-
-export default async function(weekStart: string): Promise<Entry[]> {
-  return fetch(`${REMOTE_URL}/weeks/${weekStart}`).then(res => res.json())
-}
--- old-client/src/getOverview.ts
+++
@@ -1,5 +0,0 @@
-const REMOTE_URL = "http://localhost:3001"
-
-export default async function(): Promise<{[day: string]: number | undefined}> {
-  return fetch(`${REMOTE_URL}/overview`).then(res => res.json())
-}
