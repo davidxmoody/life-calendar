4ecfc34aaddf95a2b5d4250751320bf044d5eba3
David Moody <david@davidxmoody.com>
2020-09-26 14:09:46 +0100

    Format layers in a more readable way

--- client/src/components/LayerList.tsx
+++ client/src/components/LayerList.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-useless-escape */
+
 import * as React from "react"
 import {useQuery} from "react-query"
 import fetchLayerList from "../api/fetchLayerList"
@@ -14,26 +16,43 @@ export default function LayerList(props: Props) {
     return null
   }
 
+  const groups: Record<string, string[] | undefined> = {}
+  data.forEach((fullName) => {
+    const groupName = fullName.includes("/") ? fullName.replace(/\/.*/, "") : ""
+    const name = fullName.replace(/[^\/]*\//, "")
+    groups[groupName] = [...(groups[groupName] ?? []), name]
+  })
+
   return (
     <div style={{textOverflow: "wrap"}}>
-      {data.map((layerName) => (
-        <span
-          key={layerName}
-          onClick={() => props.setLayerName(layerName)}
-          style={{
-            display: "inline-block",
-            marginRight: 8,
-            marginBottom: 4,
-            fontStyle:
-              props.activeLayerName === layerName ? "italic" : "normal",
-            textDecoration:
-              props.activeLayerName === layerName ? "none" : "underline",
-            color: "blue",
-            cursor: "pointer",
-          }}
-        >
-          {layerName}
-        </span>
+      {Object.keys(groups).map((groupName) => (
+        <p key={groupName}>
+          {`${groupName}: `}
+          {groups[groupName]!.map((name) => (
+            <span
+              key={name}
+              onClick={() =>
+                props.setLayerName(groupName ? `${groupName}/${name}` : name)
+              }
+              style={{
+                display: "inline-block",
+                marginRight: 8,
+                fontStyle:
+                  props.activeLayerName === `${groupName}/${name}`
+                    ? "italic"
+                    : "normal",
+                textDecoration:
+                  props.activeLayerName === `${groupName}/${name}`
+                    ? "none"
+                    : "underline",
+                color: "blue",
+                cursor: "pointer",
+              }}
+            >
+              {name}
+            </span>
+          ))}
+        </p>
       ))}
     </div>
   )
