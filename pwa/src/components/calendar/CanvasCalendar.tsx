77eca0871db090887a3492f240b940ebd7839dc7
David Moody <david@davidxmoody.com>
2021-01-26 19:31:46 +0000

    Draw calendar on pixel boundaries

--- pwa/src/components/calendar/CanvasCalendar.tsx
+++ pwa/src/components/calendar/CanvasCalendar.tsx
@@ -8,21 +8,34 @@ export default memo(function Calendar() {
   // const [, setLocation] = useLocation()
   const today = useToday()
   // const layerData = useLayerData(props.layerId)
-  const calendarData = useMemo(
-    () => generateCalendarData({today, ...lifeData}),
-    [today],
-  )
+  const data = useMemo(() => generateCalendarData({today, ...lifeData}), [
+    today,
+  ])
 
   const ref = useRef<HTMLCanvasElement>(null)
+
+  const canvasWidth = window.innerWidth
+  const canvasHeight = canvasWidth * 1.6
+
+  const pixelRatio = window.devicePixelRatio || 1
+  const drawWidth = pixelRatio * canvasWidth
+  const drawHeight = pixelRatio * canvasHeight
 
   useEffect(() => {
     if (ref.current) {
       const ctx = ref.current.getContext("2d")
       if (ctx) {
-        drawCalendar(ctx, calendarData)
+        drawCalendar({ctx, data, width: drawWidth, height: drawHeight})
       }
     }
-  }, [ref, calendarData])
+  }, [ref.current, data, drawWidth, drawHeight])
 
-  return <canvas width={331} height={1000} ref={ref} />
+  return (
+    <canvas
+      ref={ref}
+      width={drawWidth}
+      height={drawHeight}
+      style={{width: canvasWidth, height: canvasHeight}}
+    />
+  )
 })
--- pwa/src/components/calendar/drawCalendar.ts
+++ pwa/src/components/calendar/drawCalendar.ts
@@ -1,25 +1,48 @@
 import {CalendarData} from "../../helpers/generateCalendarData"
 
-export default function (ctx: CanvasRenderingContext2D, data: CalendarData) {
+export default function ({
+  ctx,
+  data,
+  width,
+  height,
+}: {
+  ctx: CanvasRenderingContext2D
+  data: CalendarData
+  width: number
+  height: number
+}) {
   console.time("drawCalendar")
+  ctx.save()
 
-  const yearMargin = 1
-  const yearHeight = 45
-  const yearWidth = 30
+  ctx.clearRect(0, 0, width, height)
 
-  const weekMargin = 1
-  const weekWidth = 4
-  const weekHeight = 4
+  const maxWidthPerYear = Math.floor(width / 10)
 
+  const yearMargin = Math.ceil(maxWidthPerYear / 20)
   const numWeeksPerRow = 6
+
+  const weekWidthIncMargin = Math.floor(
+    (maxWidthPerYear - yearMargin * 2) / numWeeksPerRow,
+  )
+  const yearWidthIncMargin =
+    weekWidthIncMargin * numWeeksPerRow + 2 * yearMargin
+
+  const yearHeightIncMargin =
+    weekWidthIncMargin * Math.ceil(53 / numWeeksPerRow) + 2 * yearMargin
+
+  const calendarWidth = yearWidthIncMargin * 10
+  const leftOffset = Math.floor((width - calendarWidth + yearMargin) / 2)
+  ctx.translate(leftOffset, 0)
+
+  const weekMargin = 2
 
   data.decades.forEach((decade, decadeIndex) => {
     ctx.save()
-    ctx.translate(0, (yearHeight + 2 * yearMargin) * decadeIndex)
+    ctx.translate(0, yearHeightIncMargin * decadeIndex)
 
     decade.years.forEach((year, yearIndex) => {
       ctx.save()
-      ctx.translate((yearWidth + 2 * yearMargin) * yearIndex, 0)
+      ctx.translate(yearWidthIncMargin * yearIndex, 0)
 
       year.weeks.forEach((week, weekIndex) => {
         const weekX = weekIndex % numWeeksPerRow
@@ -28,10 +51,10 @@ export default function (ctx: CanvasRenderingContext2D, data: CalendarData) {
         ctx.fillStyle =
           "era" in week ? week.era.color : `rgba(200, 200, 200, ${week.prob})`
         ctx.fillRect(
-          weekX * (weekWidth + weekMargin),
-          weekY * (weekHeight + weekMargin),
-          weekWidth,
-          weekHeight,
+          weekX * weekWidthIncMargin,
+          weekY * weekWidthIncMargin,
+          weekWidthIncMargin - weekMargin,
+          weekWidthIncMargin - weekMargin,
         )
       })
 
@@ -41,5 +64,6 @@ export default function (ctx: CanvasRenderingContext2D, data: CalendarData) {
     ctx.restore()
   })
 
+  ctx.restore()
   console.timeEnd("drawCalendar")
 }
