481f699bb1402c6568729779166be2fc31a18a86
David Moody <david@davidxmoody.com>
2021-04-24 11:19:01 +0100

    Update deps, remove unused deps

--- pwa/src/index.tsx
+++ pwa/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./components/App"
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
-import reportWebVitals from "./reportWebVitals"
 import {ChakraProvider, ColorModeScript} from "@chakra-ui/react"
 
 ReactDOM.render(
@@ -20,8 +19,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
