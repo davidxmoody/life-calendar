1da219ebccd27134d06049aa62e759d48e8601ec
David Moody <david@davidxmoody.com>
2021-04-25 08:51:55 +0100

    Reduce unnecessary calendar redraws

--- pwa/src/components/calendar/Calendar.tsx
+++ pwa/src/components/calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, memo, useRef, useEffect, useState} from "react"
+import React, {useMemo, memo, useRef, useEffect} from "react"
 import generateCalendarData from "../../helpers/generateCalendarData"
 import lifeData from "../../lifeData"
 import useToday from "../../hooks/useToday"
@@ -16,9 +16,6 @@ interface Props {
 export default memo(function Calendar(props: Props) {
   const [, setLocation] = useLocation()
 
-  const [selectedYearIndex /*, setSelectedYearIndex*/] = useState<
-    number | null
-  >(null)
   const today = useToday()
   const layerData = useLayerData(props.layerId)
   const data = useMemo(() => generateCalendarData({today, ...lifeData}), [
@@ -34,10 +31,14 @@ export default memo(function Calendar(props: Props) {
   const drawWidth = pixelRatio * canvasWidth
   const drawHeight = pixelRatio * canvasHeight
 
-  const d = calculateCalendarDimensions({
-    width: drawWidth,
-    height: drawHeight,
-  })
+  const d = useMemo(
+    () =>
+      calculateCalendarDimensions({
+        width: drawWidth,
+        height: drawHeight,
+      }),
+    [drawWidth, drawHeight],
+  )
 
   useEffect(() => {
     if (ref.current) {
@@ -48,93 +49,36 @@ export default memo(function Calendar(props: Props) {
     }
   }, [ref.current, data, layerData, d])
 
-  const transform = selectedYearIndex != null ? `scale(6)` : "scale(1)"
-  const transformOrigin =
-    selectedYearIndex != null
-      ? `${(selectedYearIndex % 10) * 11}% ${
-          Math.floor(selectedYearIndex / 10) * 11
-        }%`
-      : undefined
-  const lastTransformOrigin = useRef(transformOrigin)
-  if (transformOrigin) {
-    lastTransformOrigin.current = transformOrigin
-  }
-
   return (
     <Box display="flex" justifyContent="center" overflow="hidden">
-      <Box
-        transform={transform}
-        transformOrigin={lastTransformOrigin.current}
-        transition="transform 0.5s ease-out"
-      >
-        <canvas
-          ref={ref}
-          width={drawWidth}
-          height={drawHeight}
-          style={{width: canvasWidth, height: canvasHeight, cursor: "pointer"}}
-          // onMouseMove={(e) => {
-          //   const rect = e.currentTarget.getBoundingClientRect()
-          //   const x = (e.pageX - window.pageXOffset - rect.left) * pixelRatio
-          //   const y = (e.pageY - window.pageYOffset - rect.top) * pixelRatio
+      <canvas
+        ref={ref}
+        width={drawWidth}
+        height={drawHeight}
+        style={{width: canvasWidth, height: canvasHeight, cursor: "pointer"}}
+        onClick={(e) => {
+          const rect = e.currentTarget.getBoundingClientRect()
+          const x = (e.pageX - window.pageXOffset - rect.left) * pixelRatio
+          const y = (e.pageY - window.pageYOffset - rect.top) * pixelRatio
 
-          //   const result = getWeekUnderCursor({x, y, d})
+          const c = getWeekUnderCursor({x, y, d})
 
-          //   if (!result) {
-          //     return
-          //   }
+          if (!c) {
+            return
+          }
 
-          //   const {rowIndex, colIndex, weekRowIndex, weekColIndex} = result
+          const week =
+            data.decades[c.colIndex].years[c.rowIndex].weeks[
+              c.weekColIndex * d.layout.weeksPerYearRow + c.weekRowIndex
+            ]
 
-          //   const context = ref.current?.getContext("2d")
-          //   if (context) {
-          //     context.save()
-          //     context.fillStyle = "rgba(200, 0, 0, 0.02)"
-          //     context.fillRect(
-          //       d.canvas.px +
-          //         rowIndex * d.year.w +
-          //         d.year.p +
-          //         weekRowIndex * d.week.w,
-          //       d.canvas.py +
-          //         colIndex * d.year.h +
-          //         d.year.p +
-          //         weekColIndex * d.week.h,
-          //       d.week.w,
-          //       d.week.h,
-          //     )
-          //     context.restore()
-          //   }
-          // }}
-          onClick={(e) => {
-            // if (selectedYearIndex != null) {
-            //   setSelectedYearIndex(null)
-            //   return
-            // }
+          if (!week || week.startDate > today) {
+            return
+          }
 
-            const rect = e.currentTarget.getBoundingClientRect()
-            const x = (e.pageX - window.pageXOffset - rect.left) * pixelRatio
-            const y = (e.pageY - window.pageYOffset - rect.top) * pixelRatio
-
-            const c = getWeekUnderCursor({x, y, d})
-
-            if (!c) {
-              return
-            }
-
-            // setSelectedYearIndex(underCursor.yearIndex)
-
-            const week =
-              data.decades[c.colIndex].years[c.rowIndex].weeks[
-                c.weekColIndex * d.layout.weeksPerYearRow + c.weekRowIndex
-              ]
-
-            if (!week || week.startDate > today) {
-              return
-            }
-
-            setLocation(`/weeks/${week.startDate}`)
-          }}
-        />
-      </Box>
+          setLocation(`/weeks/${week.startDate}`)
+        }}
+      />
     </Box>
   )
 })
