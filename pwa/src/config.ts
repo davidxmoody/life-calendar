328551b1d8fcefbafd1247c626f057f8d4a16ab2
David Moody <david@davidxmoody.com>
2022-04-14 12:52:54 +0100

    Add hacky runtime configurable remote url config

--- pwa/src/config.ts
+++ pwa/src/config.ts
@@ -1,1 +1,11 @@
-export const REMOTE_URL = window.location.origin.replace(/:\d+$/, ":8051")
+function isValidRemoteUrl(url: string | undefined | null) {
+  return /^https:\/\/.*$/.test(url ?? "")
+}
+
+while (!isValidRemoteUrl(localStorage.REMOTE_URL)) {
+  localStorage.REMOTE_URL = prompt(
+    "Please enter remote server URL (e.g. 'https://192.168.0.26:8051')",
+  )
+}
+
+export const REMOTE_URL = localStorage.REMOTE_URL
