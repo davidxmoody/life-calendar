535cf2af4470d3d1f21b27a56e55d58a1ff7a560
David Moody <david@davidxmoody.com>
2022-04-21 14:08:50 +0100

    Set initial color mode to dark

--- pwa/src/index.tsx
+++ pwa/src/index.tsx
@@ -3,19 +3,24 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./components/App"
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
-import {ChakraProvider, ColorModeScript} from "@chakra-ui/react"
+import {ChakraProvider, ColorMode, ColorModeScript} from "@chakra-ui/react"
 import {extendTheme} from "@chakra-ui/react"
 import {Provider as JotaiProvider} from "jotai"
+
+const initialColorMode: ColorMode = "dark"
 
 const theme = extendTheme({
   shadows: {
     outline: "none",
   },
+  config: {
+    initialColorMode,
+  },
 })
 
 ReactDOM.render(
   <StrictMode>
-    <ColorModeScript />
+    <ColorModeScript initialColorMode={initialColorMode} />
     <ChakraProvider theme={theme}>
       <JotaiProvider>
         <App />
