107fa94f7e830cdcf24f00b5ebd2ea71ca8133ca
David Moody <david@davidxmoody.com>
2022-04-21 12:21:10 +0100

    Remove while loop in config

--- pwa/src/config.ts
+++ pwa/src/config.ts
@@ -2,7 +2,7 @@ function isValidRemoteUrl(url: string | undefined | null) {
   return /^https:\/\/.*$/.test(url ?? "")
 }
 
-while (!isValidRemoteUrl(localStorage.REMOTE_URL)) {
+if (!isValidRemoteUrl(localStorage.REMOTE_URL)) {
   localStorage.REMOTE_URL = prompt(
     "Please enter remote server URL (e.g. 'https://192.168.0.26:8051')",
   )
