391033c2e20a3e97568ecf7871491f0011c22b92
David Moody <david@davidxmoody.com>
2022-04-26 10:36:50 +0100

    Update react-markdown

--- pwa/src/components/entries/Markdown.tsx
+++ pwa/src/components/entries/Markdown.tsx
@@ -1,8 +1,15 @@
-import React, {ElementType, memo, ReactNode} from "react"
-import ReactMarkdown from "react-markdown"
+import React, {memo} from "react"
+import ReactMarkdown, {Components} from "react-markdown"
 import {Link as WouterLink} from "wouter"
 import {getWeekStart} from "../../helpers/dates"
-import {Box, Heading, Link, Text} from "@chakra-ui/react"
+import {
+  Box,
+  Heading,
+  Link,
+  OrderedList,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react"
 
 interface Props {
   source: string
@@ -11,75 +18,79 @@ interface Props {
 const paraMarginBottom = 5
 const codeBackgroudColor = "rgba(120, 120, 120, 0.5)"
 
-const renderers: Record<string, ElementType> = {
-  link: (props: {href: string; children: ReactNode}) => {
-    if (props.href.startsWith("/")) {
-      return (
-        <Link as={WouterLink} href={props.href} color="teal.500">
-          {props.children}
-        </Link>
-      )
-    }
-    return (
-      <Link href={props.href} isExternal color="teal.500">
-        {props.children}
-      </Link>
-    )
-  },
-  paragraph: (props: any) => <Text mb={paraMarginBottom} {...props} />,
-  heading: (props: {level: number; children: any}) => (
-    <Heading
-      size={props.level <= 2 ? "xl" : props.level === 3 ? "lg" : "md"}
-      mb={4}
-    >
+function heading(size: React.ComponentProps<typeof Heading>["size"]) {
+  return (props: {children: any}) => (
+    <Heading size={size} mb={4}>
       {props.children}
     </Heading>
-  ),
-  blockquote: (props: any) => (
+  )
+}
+
+const components: Components = {
+  a: (props) =>
+    props.href?.startsWith("/") ? (
+      <Link as={WouterLink} href={props.href} color="teal.500">
+        {props.children}
+      </Link>
+    ) : (
+      <Link href={props.href} isExternal rel="noreferrer" color="teal.500">
+        {props.children}
+      </Link>
+    ),
+  p: (props) => <Text mb={paraMarginBottom} {...props} />,
+  h1: heading("xl"),
+  h2: heading("xl"),
+  h3: heading("lg"),
+  h4: heading("md"),
+  h5: heading("md"),
+  h6: heading("md"),
+  blockquote: (props) => (
     <Text
       pl={4}
       fontStyle="italic"
       borderColor={codeBackgroudColor}
       borderLeftWidth="8px"
-      opacity={0.7}
-      {...props}
-    />
-  ),
-  image: (props: any) => (
-    <img style={{maxWidth: "100%"}} src={props.src} alt={props.alt} />
-  ),
-  list: (props: any) => (
-    <Box mb={paraMarginBottom} pl={5}>
-      {React.createElement(ReactMarkdown.renderers.list as any, props)}
-    </Box>
-  ),
-  code: (props: {value: any}) => (
-    <Box
-      display="block"
-      as="code"
-      mb={paraMarginBottom}
-      backgroundColor={codeBackgroudColor}
-      py={2}
-      px={3}
-      borderRadius={3}
-    >
-      {props.value}
-    </Box>
-  ),
-  inlineCode: (props: {children: any}) => (
-    <Box
-      as="code"
-      backgroundColor={codeBackgroudColor}
-      borderRadius="sm"
-      px={1}
+      opacity={0.75}
     >
       {props.children}
-    </Box>
+    </Text>
   ),
+  img: (props) => (
+    <img style={{maxWidth: "100%"}} src={props.src} alt={props.alt} />
+  ),
+  ol: (props) => (
+    <OrderedList mb={paraMarginBottom}>{props.children}</OrderedList>
+  ),
+  ul: (props) => (
+    <UnorderedList mb={paraMarginBottom}>{props.children}</UnorderedList>
+  ),
+  code: (props) =>
+    props.inline ? (
+      <Box
+        as="code"
+        backgroundColor={codeBackgroudColor}
+        borderRadius="sm"
+        px={1}
+      >
+        {props.children}
+      </Box>
+    ) : (
+      <Box
+        display="block"
+        as="code"
+        mb={paraMarginBottom}
+        backgroundColor={codeBackgroudColor}
+        py={2}
+        px={3}
+        borderRadius={3}
+      >
+        {props.children}
+      </Box>
+    ),
 }
 
 function addDateLinks(source: string): string {
-  return source.replaceAll(
+  return source.replace(
     /\b\d{4}-\d{2}-\d{2}\b/g,
     (match) => `[${match}](/weeks/${getWeekStart(match)})`,
   )
@@ -87,7 +98,7 @@ function addDateLinks(source: string): string {
 
 export default memo(function Markdown(props: Props) {
   return (
-    <ReactMarkdown renderers={renderers}>
+    <ReactMarkdown components={components}>
       {addDateLinks(props.source)}
     </ReactMarkdown>
   )
