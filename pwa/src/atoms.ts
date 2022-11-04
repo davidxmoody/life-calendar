aa1cc0d378de07455737e1f07bccf2b568cdbf27
David Moody <david@davidxmoody.com>
2022-11-04 08:10:10 +0000

    Add search layer generation

--- pwa/src/atoms.ts
+++ pwa/src/atoms.ts
@@ -2,7 +2,7 @@
 
 import {atom} from "jotai"
 import {atomWithStorage} from "jotai/utils"
-import {uniq} from "ramda"
+import {uniq, countBy, map} from "ramda"
 import {
   getEntriesForDay,
   getHeadingsInRange,
@@ -20,6 +20,7 @@ import {
   parseYear,
 } from "./helpers/dates"
 import {DayHeadings} from "./helpers/getHeadings"
+import {LayerData} from "./types"
 
 export const nullAtom = atom(null)
 
@@ -45,12 +46,37 @@ export const updateTriggerAtom = atom(0)
 
 export const layerIdsAtom = atom(async (get) => {
   get(updateTriggerAtom)
-  return getLayerIds()
+  return [...(await getLayerIds()), "search"]
 })
 
 export const selectedLayerDataAtom = atom(async (get) => {
   get(updateTriggerAtom)
   const selectedLayerId = get(selectedLayerIdAtom)
+  const searchRegex = get(searchRegexAtom)
+  const lifeData = get(lifeDataAtom)
+
+  if (selectedLayerId === "search") {
+    if (!searchRegex || !lifeData) {
+      return null
+    }
+
+    console.log("Running full search to generate layer")
+    const searchResults = await searchDb(searchRegex, {
+      startInclusive: lifeData.birthDate,
+      endExclusive: lifeData.deathDate,
+    })
+
+    const weekCounts = countBy(
+      (x) => x,
+      searchResults.map((r) => getWeekStart(r.date)),
+    )
+    return Object.fromEntries(
+      Object.entries(weekCounts).map(([date, count]) => [
+        date,
+        Math.min(1, Math.pow(count / 7, 0.5)),
+      ]),
+    )
+  }
 
   return selectedLayerId ? getLayerData(selectedLayerId) : null
 })
