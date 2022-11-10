b1a51d69280d9a3d0bc5f216845607bd41b175a7
David Moody <david@davidxmoody.com>
2022-11-10 09:47:00 +0000

    Change Suspense boundaries to load nav faster

--- pwa/src/components/nav/NavBar.tsx
+++ pwa/src/components/nav/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, {memo} from "react"
+import React, {memo, Suspense} from "react"
 import {Box, Flex} from "@chakra-ui/react"
 import LayerList from "./LayerList"
 import SyncButton from "./SyncButton"
@@ -10,7 +10,7 @@ export const NAV_BAR_HEIGHT_PX = 72
 export default memo(function NavBar() {
   return (
     <Box
-      height={`${NAV_BAR_HEIGHT_PX}px`}
+      minHeight={`${NAV_BAR_HEIGHT_PX}px`}
       display="flex"
       alignItems="center"
       p={4}
@@ -18,21 +18,23 @@ export default memo(function NavBar() {
       zIndex="banner"
       flex={0}
     >
-      <Box display={{base: "flex", md: "none"}} mr={4}>
-        <MobileViewSwitcher />
-      </Box>
+      <Suspense>
+        <Box display={{base: "flex", md: "none"}} mr={4}>
+          <MobileViewSwitcher />
+        </Box>
 
-      <Box mr={4}>
-        <LayerList />
-      </Box>
+        <Box mr={4}>
+          <LayerList />
+        </Box>
 
-      <Flex flex={1} justifyContent="flex-end" minWidth="100px">
-        <SearchButton />
-      </Flex>
+        <Flex flex={1} justifyContent="flex-end" minWidth="100px">
+          <SearchButton />
+        </Flex>
 
-      <Box ml={4}>
-        <SyncButton />
-      </Box>
+        <Box ml={4}>
+          <SyncButton />
+        </Box>
+      </Suspense>
     </Box>
   )
 })
