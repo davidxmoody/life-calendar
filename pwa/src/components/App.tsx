1210f92f4d620155af7e7bab29c6a46d2d9adb5d
David Moody <david@davidxmoody.com>
2022-11-17 09:00:32 +0000

    Transition timeline opacity (also fix scroll bug)

--- pwa/src/components/App.tsx
+++ pwa/src/components/App.tsx
@@ -2,7 +2,7 @@ import Timeline from "./timeline/Timeline"
 import NavBar from "./nav/NavBar"
 import {Box, Flex} from "@chakra-ui/react"
 import Calendar from "./calendar/Calendar"
-import {Suspense} from "react"
+import {ComponentProps, Suspense} from "react"
 import {useAtomValue} from "jotai"
 import {mobileViewAtom} from "../atoms"
 import FirstTimeSetupModal from "./FirstTimeSetupModal"
@@ -16,24 +16,12 @@ export default function App() {
         <Flex height="100vh" flexDirection="column">
           <NavBar />
 
-          <Flex flex={1} overflowY="auto">
-            <Box
-              flex={0}
-              display={{
-                base: mobileView === "calendar" ? "block" : "none",
-                md: "block",
-              }}
-            >
+          <Flex flex={1} overflow="hidden" position="relative">
+            <Box flex={0} {...mobileVisibility(mobileView === "calendar")}>
               <Calendar />
             </Box>
 
-            <Box
-              flex={1}
-              display={{
-                base: mobileView === "timeline" ? "block" : "none",
-                md: "block",
-              }}
-            >
+            <Box flex={1} {...mobileVisibility(mobileView === "timeline")}>
               <Timeline />
             </Box>
           </Flex>
@@ -42,4 +30,19 @@ export default function App() {
       <FirstTimeSetupModal />
     </>
   )
+}
+
+function mobileVisibility(
+  visible: boolean,
+): Partial<ComponentProps<typeof Box>> {
+  return {
+    position: {base: "absolute", md: "static"},
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    opacity: {base: visible ? 1 : 0, md: 1},
+    pointerEvents: {base: visible ? "all" : "none", md: "all"},
+    transition: "opacity 0.3s",
+  }
 }
