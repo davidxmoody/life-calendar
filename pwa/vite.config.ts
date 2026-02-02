0969ea133d95daeefdae44a384956a3c7711b247
David Moody <david@davidxmoody.com>
2026-02-02 13:26:42 +0000

    Remove HTTPS config from PWA

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from "vitest/config"
 import react from "@vitejs/plugin-react"
 import {VitePWA} from "vite-plugin-pwa"
-import fs from "fs"
 
 export default defineConfig({
   plugins: [
@@ -11,16 +10,6 @@ export default defineConfig({
       manifest: false,
     }),
   ],
-  server: {
-    https: {
-      key: fs.readFileSync(process.env.LOCAL_SSL_KEY || ""),
-      cert: fs.readFileSync(process.env.LOCAL_SSL_CERT || ""),
-    },
-    port: 3000,
-  },
-  build: {
-    outDir: "build",
-  },
   test: {
     globals: true,
   },
